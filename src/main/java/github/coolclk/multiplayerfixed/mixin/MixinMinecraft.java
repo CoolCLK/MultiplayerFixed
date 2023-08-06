110165032b66333d5df49d99c925ad11b0bfbc7a
CoolCLK <mr.chenlk123@qq.com>
2023-08-06 20:32:51 +0800

    Fixed chat limit

--- src/main/java/github/coolclk/multiplayerfixed/mixin/MixinMinecraft.java
+++ src/main/java/github/coolclk/multiplayerfixed/mixin/MixinMinecraft.java
@@ -1,6 +1,7 @@
 package github.coolclk.multiplayerfixed.mixin;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,8 +9,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
+    @Inject(at = @At(value = "RETURN"), method = "createTitle", cancellable = true)
+    public void createTitle(CallbackInfoReturnable<String> cir) {
+        cir.setReturnValue(I18n.get("title.multiplayer.fixed", cir.getReturnValue()));
+    }
+
     @Inject(at = @At(value = "RETURN"), method = "allowsMultiplayer", cancellable = true)
     public void allowsMultiplayer(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+
+    @Inject(at = @At(value = "RETURN"), method = "allowsChat", cancellable = true)
+    public void allowsChat(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(true);
     }
 }
